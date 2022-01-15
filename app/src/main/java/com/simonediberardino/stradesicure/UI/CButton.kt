d1d256b5fa4724ce4673811d405eda8e8f2fa28b
diberardinocalisti <diberardinocalisti@gmail.com>
2022-01-15 11:31:59 +0100

    logout btn

---
+++ app/src/main/java/com/simonediberardino/stradesicure/UI/CButton.kt
@@ -0,0 +1,92 @@
+package com.simonediberardino.stradesicure.UI
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.simonediberardino.stradesicure.R as R
+import com.simonediberardino.stradesicure.misc.RunnablePar
+import android.content.DialogInterface
+
+import android.R as R2
+
+
+
+
+@SuppressLint("ViewConstructor")
+open class CButton(val context: AppCompatActivity, val parent: ViewGroup) : View(context) {
+    private var titleTV: TextView
+    private var descriptionTV: TextView
+    private var content: View
+    private var onConfirmListener: Runnable
+    private var onDenyListener: Runnable
+
+    var options: Array<String>? = null
+
+    var title: String = String()
+        get() {
+            return String()
+        }
+        set(value){
+            field = value
+            titleTV.text = value
+        }
+
+    var description: String = String()
+        get() {
+            return String()
+        }
+        set(value){
+            field = value
+            descriptionTV.text = value
+        }
+
+    fun setOnConfirmListener(onConfirmListener: Runnable){
+        this.onConfirmListener = onConfirmListener
+    }
+
+    fun setOnDenyListener(onDenyListener: Runnable){
+        this.onDenyListener = onDenyListener
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun showConfirmDialog(){
+
+        AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(
+                context.getString(R.string.logout_confirm)
+            )
+            .setPositiveButton(
+                context.getString(R.string.conferma)
+            ) { _, _ ->
+                this.onConfirmListener.run()
+            }
+            .setNegativeButton(
+                resources.getString(R.string.annulla)
+            ) { _, _ ->
+
+            }.show()
+    }
+
+    fun apply() {
+        parent.addView(content)
+    }
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        val layoutToAdd: Int = R.layout.long_button
+
+        content = inflater.inflate(layoutToAdd, parent, false)
+        titleTV = content.findViewById(R.id.button_title)
+        descriptionTV = content.findViewById(R.id.button_description)
+        options = arrayOf(context.getString(R.string.conferma), context.getString(R.string.annulla))
+        onConfirmListener = Runnable {}
+        onDenyListener = Runnable {}
+        content.setOnClickListener { showConfirmDialog() }
+    }
+}
--- app/src/main/java/com/simonediberardino/stradesicure/activities/SettingsActivity.kt
+++ app/src/main/java/com/simonediberardino/stradesicure/activities/SettingsActivity.kt
@@ -2,21 +2,41 @@ package com.simonediberardino.stradesicure.activities
 
 import android.view.ViewGroup
 import com.simonediberardino.stradesicure.R
+import com.simonediberardino.stradesicure.UI.CButton
 import com.simonediberardino.stradesicure.UI.CSpinner
+import com.simonediberardino.stradesicure.login.LoginHandler
+import com.simonediberardino.stradesicure.utils.Utility
 
 class SettingsActivity : AdaptedActivity() {
     override fun initializeLayout() {
         setContentView(R.layout.activity_settings)
         this.setupMapThemeSpinner()
+        this.setupLogoutBtn()
     }
 
     private fun setupMapThemeSpinner(){
         val mainLayout = findViewById<ViewGroup>(R.id.settings_viewgroup_1)
-        var spinner = CSpinner(this, mainLayout)
+        val spinner = CSpinner(this, mainLayout)
 
         spinner.options = resources.getStringArray(R.array.maptheme)
         spinner.title = getString(R.string.mapstyle)
 
         spinner.apply()
+    }
+
+    private fun setupLogoutBtn(){
+        if(!LoginHandler.isLoggedIn())
+            return
+
+        val mainLayout = findViewById<ViewGroup>(R.id.settings_viewgroup_2)
+        val button = CButton(this, mainLayout)
+
+        button.title = getString(R.string.logout)
+        button.setOnConfirmListener {
+            LoginActivity.onLogout()
+            Utility.showToast(this, getString(R.string.logout_success))
+        }
+
+        button.apply()
     }
 }
